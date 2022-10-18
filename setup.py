cd404d1104cc4a31acd15b7f514be6bfe3d8b164
RMHIVSM <rmhivsm@ucl.ac.uk>
2022-10-18 09:55:07 +0100

    Cleaning and adding readme

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-
 setup(
     name="pk_aug",
     packages=find_packages(),
